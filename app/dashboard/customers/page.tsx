43f2eb3137bff942975a06c08123097961ba0cbd
TheJobGG <jobglez80@gmail.com>
2024-03-10 15:58:50 -0600

    Add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+  description: 'Page with the customers overview'
+}
+
 export default function Page() {
   return <p className="text-2xl text-center">Customers Page</p>;
 }
